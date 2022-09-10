b2d6a48d6a42677620b437e67f1c4206b35114f9
FRomanY <113242369+FRomanY@users.noreply.github.com>
2022-09-10 14:59:11 +0300

    Add files via upload

---
+++ meta/js/app.js
@@ -0,0 +1,12 @@
+$( function() {
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0.1,
+    cssEase: 'linear',
+    pauseOnFocus:false,
+    pauseOnHover:false,
+   });
+
+});
